29b5d510452a28622309b0a0fd10e7f7d0dc5011
lesg1151860 <luiseduardosg@ufps.edu.co>
2026-06-01 19:46:24 -0500

    feat: agregar nuevas rutas para la API de autenticación en la configuración de URLs

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
-    path('api/', include('accounts.urls')),  # Agrega esta línea para incluir las URLs de accounts
+    path('api/auth/', include('accounts.urls')),
+    path('api/', include('accounts.urls')), # <-- Nueva ruta para registro
 ]
-    
