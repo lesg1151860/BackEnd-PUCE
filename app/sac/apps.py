72017dbb37ca5472a7fa61d3c01828ce4ec38ec9
lesg1151860 <luiseduardosg@ufps.edu.co>
2026-06-01 19:56:47 -0500

    delete: eliminación de archivos y migraciones obsoletos del sistema

--- app/sac/apps.py
+++
@@ -1,7 +0,0 @@
-# app/sac/apps.py
-from django.apps import AppConfig
-
-
-class SacConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app.sac'  # <--- Debe ser 'app.sac'
--- app/siuce/apps.py
+++
@@ -1,7 +0,0 @@
-# app/siuce/apps.py
-from django.apps import AppConfig
-
-
-class SiuceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app.siuce'  # <--- Debe ser 'app.siuce'
