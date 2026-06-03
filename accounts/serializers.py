3de6427ceaa47ee191e514575f19c71090453c0e
lesg1151860 <luiseduardosg@ufps.edu.co>
2026-06-02 21:01:33 -0500

    feat: simplificar la creación de usuarios en el serializer de RegistroUsuario

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -15,29 +15,26 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class RegistroUsuarioSerializer(serializers.ModelSerializer):
     
+    is_staff = serializers.BooleanField(required=False)
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
         model = Usuario
         fields = ('id','username', 'email', 'first_name', 'last_name', 'password','is_staff') 
-        extra_kwargs = {
-            'is_staff': {'read_only': True}
-        }
 
     def create(self, validated_data):
-        user = Usuario(
-            username=validated_data['username'],
-            email=validated_data.get('email', ''),
-            first_name=validated_data.get('first_name', ''),
-            last_name=validated_data.get('last_name', '')
-        )
-        user.set_password(validated_data['password'])
+        es_admin = validated_data.pop('is_staff', False)
+        password = validated_data.pop('password')
         
-        if validated_data.get('is_staff') == True:
+        user = Usuario(**validated_data)
+        user.set_password(password)
+        
+        if es_admin:
             user.is_staff = True
             user.is_superuser = True
         else:
             user.is_staff = False
             user.is_superuser = False
+            
         user.save()
         return user
