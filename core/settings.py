1c0c09cf2039f9f2baf98bccdb8b68d313650f18
lesg1151860 <luiseduardosg@ufps.edu.co>
2026-06-07 22:40:19 -0500

    fix: eliminar comentarios innecesarios en la configuración de Django

--- core/settings.py
+++ core/settings.py
@@ -14,9 +14,6 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-&f=o2ctoya)4xmbbky5kvl)5lwyrxq+pn@&_^9kqpbif6uqs#='
 
