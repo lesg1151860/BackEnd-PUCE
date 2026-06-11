a37957d541bfe114864afac2f2f6c3926258005b
lesg1151860 <luiseduardosg@ufps.edu.co>
2026-06-10 23:37:09 -0500

    feat: agregar ruta para incluir catálogos en la API

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('api/auth/', include('accounts.urls')),
     path('api/', include('accounts.urls')), # <-- Nueva ruta para registro
     path('api/', include('institucion_educativa.urls')), # Ruta para instituciones educativas
+    path('api/', include('catalogos.urls')), # Ruta para catálogos 
 ]
