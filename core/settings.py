89e298be20e57ba8901f05dcda6bcbc214cd14b0
lesg1151860 <luiseduardosg@ufps.edu.co>
2026-06-16 17:50:39 -0500

    feat: agregar configuración de permisos por defecto en REST framework para requerir autenticación

--- core/settings.py
+++ core/settings.py
@@ -112,6 +112,9 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', # Requiere login por defecto
     )
 }
 
