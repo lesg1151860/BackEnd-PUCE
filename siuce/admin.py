cbd878362d29edfd342a88631ac16c3874fa47e7
lesg1151860 <luiseduardosg@ufps.edu.co>
2026-06-16 00:29:34 -0500

    feat: actualizar el administrador de 'CasoSIUCE' con nuevos campos y mejorar la visualización

--- siuce/admin.py
+++ siuce/admin.py
@@ -4,13 +4,13 @@ from .models import CasoSIUCE
 @admin.register(CasoSIUCE)
 class CasoSIUCEAdmin(admin.ModelAdmin):
     # Campos que se calculan automáticamente en el save()
-    readonly_fields = ('avance_ie', 'avance_sem', 'avance_general', 'estado', 'fecha_registro')
+    readonly_fields = ('porcentaje_avance_ie', 'porcentaje_avance_sem', 'avance_general', 'estado_siuce', 'fecha_registro')
     
     # Visualización en la tabla de lista
-    list_display = ('radicado_sac', 'estado', 'avance_general', 'fecha_registro')
+    list_display = ('num_radicado_siuce', 'estado_siuce', 'avance_general', 'fecha_registro')
     
     # Filtros para facilitar la gestión
-    list_filter = ('estado', 'fecha_registro')
+    list_filter = ('estado_siuce', 'fecha_registro')
     
     # Buscador por radicado o víctima
-    search_fields = ('radicado_sac', 'estudiante_victima')
+    search_fields = ('num_radicado_siuce', 'estudiante_victima')
