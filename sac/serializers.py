030af0da0f3851a9863d17705fc61d46ce3711e2
lesg1151860 <luiseduardosg@ufps.edu.co>
2026-06-29 10:52:14 -0500

    feat: agregar serializador y vista para 'RespuestaSAC'

--- sac/serializers.py
+++ sac/serializers.py
@@ -3,11 +3,20 @@ from catalogos.models import EstadoSAC
 from .models import CasoSAC 
 
 class CasoSACSerializer(serializers.ModelSerializer):
+    
+    nombre_lider = serializers.CharField(source='usuario_registro.username', read_only=True)
+    
     class Meta:
         model = CasoSAC
         fields = '__all__'
+        
         
 class EstadoSACSerializer(serializers.ModelSerializer):
     class Meta:
         model = EstadoSAC
         fields = '__all__'
+
+class RespuestaSACSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CasoSAC
+        fields = ['sac_respuesta']
--- sac/views.py
+++ sac/views.py
@@ -1,13 +1,18 @@
 from rest_framework import viewsets, permissions
 from catalogos.models import EstadoSAC
 from .models import CasoSAC
-from .serializers import CasoSACSerializer, EstadoSACSerializer
+from .serializers import CasoSACSerializer, EstadoSACSerializer, RespuestaSACSerializer
 
 class CasoSACViewSet(viewsets.ModelViewSet):
     queryset = CasoSAC.objects.all().order_by('-fecha_radicacion')
     serializer_class = CasoSACSerializer
-    
+    def perform_create(self, serializer):
+        serializer.save(usuario_registro=self.request.user)
     
 class EstadoSACViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = EstadoSAC.objects.all()
     serializer_class = EstadoSACSerializer
+    
+class RespuestaSACViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = CasoSAC.objects.all()
+    serializer_class = RespuestaSACSerializer
