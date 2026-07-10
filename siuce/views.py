d2ad770d529b39d558fe4ea9888602a25207696d
lesg1151860 <luiseduardosg@ufps.edu.co>
2026-07-09 19:59:35 -0500

    feat: simplificar el ViewSet 'CasoSIUCE' y ajustar la creación de registros con el usuario

--- siuce/views.py
+++ siuce/views.py
@@ -1,20 +1,10 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .models import CasoSIUCE
 from .serializers import CasoSIUCESerializer
 
 class CasoSIUCEViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet para gestionar los casos del SIUCE.
-    """
-    queryset = CasoSIUCE.objects.all().order_by('-fecha_registro')
+    queryset = CasoSIUCE.objects.all()
     serializer_class = CasoSIUCESerializer
-    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
-        # Aquí puedes añadir lógica adicional antes de guardar si lo necesitas
-        serializer.save()
-
-    def perform_update(self, serializer):
-        # Esto asegura que el método save() del modelo se ejecute al actualizar,
-        # lo que dispara los cálculos de porcentaje y estado automáticamente.
-        serializer.save()
+        serializer.save(usuario_registro_siuce=self.request.user)
