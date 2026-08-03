6bf408fae9eb01eaa6e1f03e97fcb95406cacf9d
lesg1151860 <luiseduardosg@ufps.edu.co>
2026-08-02 21:30:44 -0500

    feat: eliminar ruta redundante para registro y autenticación en urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
-    path('api/', include('accounts.urls')), #Ruta para registro y autenticación
     path('api/', include('catalogos.urls')), # Ruta para catálogos
     path('api/', include('siuce.urls')), # Ruta para casos SIUCE
 ]
