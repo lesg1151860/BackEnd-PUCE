b1f753b4174703581f9d31f3f803f789d84b032c
lesg1151860 <luiseduardosg@ufps.edu.co>
2026-08-02 21:27:42 -0500

    feat: agregar ruta para refrescar el token JWT en el archivo de URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from .views import CambiarPasswordView, MyTokenObtainPairView, RegistroUsuarioVi
 
 urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'), # Ruta para obtener el token JWT
+    path('login/refresh/', MyTokenObtainPairView.as_view(), name='token_refresh'), # Ruta para refrescar el token JWT
     path('usuarios/', UsuarioListCreateView.as_view(), name='usuarios-list-create'), # Ruta para listar y crear usuarios
     path('usuarios/<int:pk>/', UsuarioDetailView.as_view(), name='usuarios-detail'), # Ruta para obtener detalles de un usuario específico
     path('registro/', RegistroUsuarioView.as_view(), name='registro_usuario'), # Ruta de registro
